187cb94384940902a882de54ac65e02f425509d7
Zack Borges <zackartborges@gmail.com>
2021-08-01 10:34:54 -0400

    fixes some spacing in portfolio. commit before checkout

--- components/Portfolio.vue
+++ components/Portfolio.vue
@@ -1,20 +1,18 @@
 <template>
   <v-lazy>
-    <v-container class="portfolio  " align="center">
+    <v-container class="portfolio">
       <v-divider class="divider"></v-divider>
-      <h2 class="pr-6">Portfolio</h2>
-      <div class="projects flex-container">
-        <v-card class="project flex-child" elevation="2" outlined shaped tile>
-          <v-card-title>Our Pack</v-card-title>
-          <v-card-subtitle>Ruby on Rails, Vue.js, PostGreSQL</v-card-subtitle>
+      <h2 class="pr-6 pb-14">Portfolio</h2>
+      <div class="projects">
+        <h3 class="align-right">OurPack</h3>
+        <v-card class="single-project mx-auto mb-12" elevation="2" outlined shaped tile>
+          <v-card-title>Ruby on Rails, Vue.js, PostGreSQL</v-card-title>
+          <!-- <v-card-subtitle>Ruby on Rails, Vue.js, PostGreSQL</v-card-subtitle> -->
           <v-card-text>
             A Web Application that allows backpackers and hikers to log all of the gear into categories, to allow for an
             easier planning process.
           </v-card-text>
         </v-card>
-      </div>
-      <div class="flex-child">
-        <v-img src="comer-run.jpeg"></v-img>
       </div>
     </v-container>
   </v-lazy>
@@ -27,6 +25,10 @@ h2 {
   /* position: relative; */
   color: #e6f1ff;
 }
+h3 {
+  font-size: clamp(24px, 5vw, 40px);
+  color: #e6f1ff;
+}
 .divider {
   /* align: center; */
   /* vertical-align: start, center, end, baseline, stretch; */
@@ -34,17 +36,14 @@ h2 {
   /* justify-content: center; */
   /* width: 50%; */
 }
-.flex-container {
-  display: flex;
+.v-card {
+  height: 100%;
 }
-.flex-child {
-  flex: 1;
-  border: 2px solid yellow;
-}
-.flex-child:first-child {
+
+/* .flex-child:first-child {
   margin-right: 20px;
-}
-.project {
+} */
+.single-project {
   background-color: bisque;
 }
 .projects {
