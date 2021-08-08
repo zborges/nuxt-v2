ac9f5333c821e9b0bb039ebaea0faad71fff56da
Zack Borges <zackartborges@gmail.com>
2021-08-08 11:40:20 -0400

    adds all links to github and deployed projects

--- components/Portfolio.vue
+++ components/Portfolio.vue
@@ -6,7 +6,9 @@
       <div class="projects d-flex flex-wrap justify-center ">
         <div class="single-project">
           <br />
-          <h3 class="project-title">OurPack</h3>
+          <a href="https://ourpack.netlify.app/">
+            <h3 class="project-title">OurPack</h3>
+          </a>
           <v-card :elevation="6" color="#e6f1ff" class="rounded-xl project-card" outlined shaped tile>
             <v-card-title>
               Ruby on Rails, Vue.js,
@@ -20,8 +22,9 @@
               to allow for an easier planning process.
             </v-card-text>
             <v-card-actions class="icons">
-              <!-- <a href=""></a> -->
-              <v-icon>fab fa-github</v-icon>
+              <a href="https://github.com/zackartborges/capstone_project">
+                <v-icon>fab fa-github</v-icon>
+              </a>
             </v-card-actions>
           </v-card>
         </div>
@@ -37,46 +40,56 @@
             <!-- <v-card-subtitle>Ruby on Rails, Vue.js, PostGreSQL</v-card-subtitle> -->
             <v-card-text>
               An instant messaging web application. Users are able to log in, add friends and send messages in real
+              time. The Vue app will be deployed with AWS S3, while the Rails app will be deployed using an EC2
+              Instance.
               <br />
-              time. This project is in its final stages. The Vue app will be built with AWS S3, while the Rails app will
-              be deployed using an EC2 Instance.
+              This project is in its final stages.
             </v-card-text>
             <v-card-actions class="icons">
-              <v-icon>fab fa-github</v-icon>
+              <a href="https://github.com/zackartborges/corkboard_frontend">
+                <v-icon>fab fa-github</v-icon>
+              </a>
             </v-card-actions>
           </v-card>
         </div>
         <div class="single-project">
           <br />
-          <h3 class="project-title">Framework Watcher</h3>
+          <a href="https://framework-watcher.netlify.app/">
+            <h3 class="project-title">Framework Watcher</h3>
+          </a>
           <v-card :elevation="6" color="#e6f1ff" class="  rounded-xl " outlined shaped tile>
             <v-card-title>
-              Vanilla.JS HTML
+              Ruby on Rails, Vue.js
             </v-card-title>
             <!-- <v-card-subtitle>Ruby on Rails, Vue.js, PostGreSQL</v-card-subtitle> -->
             <v-card-text>
-              A simple display of the popularity of the most popular JavaScript frameworks.
-              <br />
-              Data is being collected from the Github API.
+              A simple display of popular JavaScript frameworks. Data is being collected from the GitHub API.
             </v-card-text>
             <v-card-actions class="icons">
-              <v-icon>fab fa-github</v-icon>
+              <a href="https://github.com/zackartborges/js_framework_watcher">
+                <v-icon>fab fa-github</v-icon>
+              </a>
             </v-card-actions>
           </v-card>
         </div>
         <!-- <v-img src="OurPack.png" height="200" width="150"></v-img> -->
         <div class="single-project">
           <br />
-          <h3 class="project-title">Framework Watcher</h3>
+          <h3 class="project-title">State Data Visualizer</h3>
           <v-card :elevation="6" color="#e6f1ff" class=" rounded-xl project-card" outlined shaped tile>
             <v-card-title>
               VanillaJS, HTML
             </v-card-title>
             <v-card-text>
-              A simple display of popular JavaScript frameworks. Data is being collected from the GitHub API.
+              Interactive map that displays data from a Rails API. Data was taken from a CSV file and migrated into a
+              PostGreSQL DB. The map was created using
+              <a href="datamaps.github.io">DataMaps</a>
+              .
             </v-card-text>
             <v-card-actions class="icons">
-              <v-icon>fab fa-github</v-icon>
+              <a href="https://github.com/zackartborges/rails_state_data">
+                <v-icon>fab fa-github</v-icon>
+              </a>
             </v-card-actions>
           </v-card>
         </div>
@@ -86,6 +99,9 @@
 </template>
 
 <style scoped>
+a {
+  text-decoration: none;
+}
 h2 {
   font-size: clamp(48px, 10vw, 80px);
   /* align-items: center; */
