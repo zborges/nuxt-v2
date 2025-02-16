26a3d9c5fe52ff0f95eca5471b562d7e847a93d0
Zack Borges <zackartborges@gmail.com>
2025-02-16 10:02:46 -0600

    upgrade nuxtv2 -> nuxtv3 and get local env running

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,68 +1,63 @@
-import colors from 'vuetify/es5/util/colors'
-import Vue from 'vue'
-import VueTypedJs from 'vue-typed-js'
-Vue.use(VueTypedJs)
+import Vue from "vue";
+// import VueTypedJs from 'vue-typed-js'
+// Vue.use(VueTypedJs)
 
 export default {
   // Target: https://go.nuxtjs.dev/config-target
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '',
-    title: 'Zack Borges Portfolio',
+    titleTemplate: "",
+    title: "Zack Borges Portfolio",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/z-icon.png' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/z-icon.png" }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [ 'boxicons/css/boxicons.min.css'
-  ],
+  css: ["boxicons/css/boxicons.min.css"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
-target:'static', 
+  plugins: [],
+
+  target: "static",
+
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/vuetify
-    '@nuxtjs/vuetify',
+    "@nuxtjs/vuetify",
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
+  modules: [],
+  pages: false,
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
-    customVariables: ['~/assets/variables.scss'],
-    defaultAssets: {icons: 'fa'},
+    customVariables: ["~/assets/variables.scss"],
+    defaultAssets: { icons: "fa" },
     theme: {
       themes: {
         dark: {
           primary: "#0a192f",
           slate: "#8892b0",
-          accent: colors.grey.darken3,
+          accent: "##37474F",
           secondary: "#8892b0",
-          // info: colors.teal.lighten1,
-          // warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
-    }
+          error: "#DD2C00",
+          success: "#00E676",
+        },
+      },
+    },
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
@@ -72,5 +67,7 @@ target:'static',
     // analyze: {
     //   analyzerMode: 'static'
     // }
-  }
-}
+  },
+
+  compatibilityDate: "2025-02-14",
+};
