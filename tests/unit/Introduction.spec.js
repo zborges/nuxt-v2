248ab8e0772a280543b91f9c98ff05d19b1aaad7
Zack Borges <zackartborges@gmail.com>
2025-03-20 15:13:19 -0600

    add tests

--- tests/unit/Introduction.spec.js
+++ tests/unit/Introduction.spec.js
@@ -3,7 +3,11 @@ import Introduction from "@/components/Introduction.vue";
 
 describe("Introduction.vue", () => {
   it("renders the correct title and paragraph", () => {
+    
     const wrapper = mount(Introduction);
+    expect(wrapper.find("h1").text()).toBe("Zack Borges");
+    expect(wrapper.find("h2").text()).toBe("Hello, my name is");
+    expect(wrapper.find("h3").text()).toBe("I build websites and web applications.");
 
     // Check if the title is rendered
     expect(wrapper.find("h2").text()).toBe("Hello, my name is");
